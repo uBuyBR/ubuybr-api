860a95bfb2e1e03e95f6884272f79a6876e77e9a
Victor Giudicissi <victor.giudicissi@exactaworks.com.br>
2020-06-16 12:02:41 -0300

    Criada estrutura base do projeto

---
+++ src/test/java/com/ubuybr/ubuybrapi/UbuybrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubuybr.ubuybrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbuybrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
